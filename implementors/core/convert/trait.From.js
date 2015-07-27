f9e8785577d07c5f34a4613f4e4b9f0c6238dc39
Armin Ronacher <armin.ronacher@active-4.com>
2015-07-27 11:37:57 +0200

    Built documentation

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['redis'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/std/io/error/struct.Error.html' title='std::io::error::Error'>Error</a>&gt; for <a class='struct' href='redis/struct.RedisError.html' title='redis::RedisError'>RedisError</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;Utf8Error&gt; for <a class='struct' href='redis/struct.RedisError.html' title='redis::RedisError'>RedisError</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(<a class='enum' href='redis/enum.ErrorKind.html' title='redis::ErrorKind'>ErrorKind</a>, &amp;'static <a href='http://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>)</a>&gt; for <a class='struct' href='redis/struct.RedisError.html' title='redis::RedisError'>RedisError</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(<a class='enum' href='redis/enum.ErrorKind.html' title='redis::ErrorKind'>ErrorKind</a>, &amp;'static <a href='http://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>, <a class='struct' href='http://doc.rust-lang.org/nightly/collections/string/struct.String.html' title='collections::string::String'>String</a>)</a>&gt; for <a class='struct' href='redis/struct.RedisError.html' title='redis::RedisError'>RedisError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
